539c11de01cc048b045c0b321b50ca8bd484ba64
Enoma Victor <vickjunior2002@gmail.com>
2022-03-05 22:34:40 +0100

    made corrections to the campaign controller

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,51 +1,54 @@
 class CampaignsController < ApplicationController
-  def index
+  # before_action :authenticate_user!
+
+ def index
     @campaigns = Campaign.all
-  end
+ end
 
-  def show
-    @campaign = Campaign.find(params[:id])
-  end
-  def edit
-    @campaign = Campaign.find(params[:id])
-  end
-
-  def update
-    @campaign = Campaign.find(params[:id])
-    @campaign.sent_at = DateTime.now
-
-    if @campaign.update(campaign_params)
-      redirect_to(@campaign)
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @campaign = Campaign.find(params[:id])
-    @campaign.destroy
-    flash[:success] = "Campaign successfully deleted."
-    redirect_to root_path, status: :see_other
-  end
+ def show
+ @campaign = Campaign.find(params[:id])
+ end
 
   def new
     @campaign = Campaign.new
   end
 
   def create
+    user_signed_in?
     @campaign = Campaign.new(campaign_params)
-    @campaign.sent_at = DateTime.now
 
-    if @campaign.save
+    if @campaign.save!
       redirect_to @campaign
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+   @campaign = Campaign.find(params[:id])
+ end
+
+ def update
+   @campaign = Campaign.find(params[:id])
+
+   if @campaign.update(campaign_params)
+     redirect_to(@campaign)
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+   @campaign = Campaign.find(params[:id])
+   @campaign.destroy
+   flash[:success] = "Campaign successfully deleted."
+   redirect_to root_path
+ end
+
 
   private
-  def campaign_params
-    params.require(:campaign).permit(:name, :message, :sent_at)
-  end
+
+ def campaign_params
+   params.require(:campaign).permit(:name, :message)
+ end
 end
