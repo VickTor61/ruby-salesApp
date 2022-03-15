b3dbd1a7f1b13540d74f5df3e5e04f6e1fd29387
Enoma Victor <vickjunior2002@gmail.com>
2022-03-15 22:48:12 +0100

    deploy to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_03_05_223439) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "campaigns", force: :cascade do |t|
     t.string "name"
     t.string "message"
