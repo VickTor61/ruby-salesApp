02ca0ef613e0e3b4d568536f223bf62c1d144a89
Enoma Victor <vickjunior2002@gmail.com>
2022-03-16 00:29:00 +0100

    ready for deploy to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_26_221015) do
+ActiveRecord::Schema.define(version: 2022_03_05_223439) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "campaigns", force: :cascade do |t|
     t.string "name"
@@ -18,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_02_26_221015) do
     t.datetime "sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
