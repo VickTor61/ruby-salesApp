63d771479d537d1b107d41f7bbacff3199726739
Enoma Victor <vickjunior2002@gmail.com>
2022-05-25 21:43:51 +0100

    migrate contact_lists model to test db

--- spec/models/contact_list_spec.rb
+++ spec/models/contact_list_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe ContactList, type: :model do
   let(:user) { create :user, email: 'newusermail@gmail.com' }
   let(:contact_campaign) { create(:campaign, user: user) }
-  let(:contact_list) { create(:contact_list, campaign: contact_campaign) }
+  let(:contact_list) { create(:contact_list, campaign: contact_campaign, user: user) }
 
   context 'when valid' do
     it 'has a name attribute' do
